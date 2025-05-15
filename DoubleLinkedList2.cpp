112c2f39e512274c0cd0456604562c92e3e50aa4
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-15 18:12:35 +0700

    Menambahkan fungsi retraverse dengan pengecekan list kosong

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -128,7 +128,33 @@ private:
         cout << "\nList is empty" << endl;
         return;
     }
+
+    // step 1: Mark first node as currentNode
+ Node *currentNode = START;
+
+ // step 2: Repeat until currentNode == NULL
+ cout << "\nRecords in ascending order of roll number are:\n";
+ int i = 0;
+ while (currentNode != NULL)
+ {
+    cout << i + 1 << ". " << currentNode->noMhs << " "<< endl;
+
+    // step 3: Move to next node
+    currentNode = currentNode->next;
+    i++;
  }
+
+}
+
+void retraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+    }
+
  
 
 
