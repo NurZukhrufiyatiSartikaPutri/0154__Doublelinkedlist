aa43dcf8891e485a28d62ad1fef476f8be389424
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-15 17:27:09 +0700

    mencari posisi penyisipan node di antara dua node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -49,6 +49,22 @@ private:
         if (START !=NULL)
         START->prev = newNode;
 
+                //step 6: newNode.prev =NULL
+        newNode->prev =NULL;
+        //step 7: START =newNode
+        START = newNode;
+        return; 
+    }
+
+    //insert in between node
+    //step 8: Located position for insertion 
+    Node *current = START;
+    while (current->next != NULL && current->next->noMhs < nim)
+    {
+        current = current->next;
+    }
+
+
 
 
 
