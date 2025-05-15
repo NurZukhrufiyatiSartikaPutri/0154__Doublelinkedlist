b29ac9a1c3b5b183b86950b2252cdc204d791dc2
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-15 18:22:54 +0700

    add record not found and record found

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -183,6 +183,26 @@ void retraverse()
             return;
     
         }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+           //step 1: Traverse to finf matching roll number
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+        //step 2: output result
+        if (current == NULL)
+        {
+            cout << "Record not found\n";
+        }
+        else 
+        {
+            cout << "Record found\n";
+            cout << "Roll number: " << current->noMhs << endl;
+        }
     }        
 
  
